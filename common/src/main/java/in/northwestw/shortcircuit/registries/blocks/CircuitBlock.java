e63b37031580f32e1f5bf464350336117749903d
North-West-Wind <wsyn148@gmail.com>
2025-01-27 18:30:20 +0800

    backport to 1.20[.1]

--- common/src/main/java/in/northwestw/shortcircuit/registries/blocks/CircuitBlock.java
+++ common/src/main/java/in/northwestw/shortcircuit/registries/blocks/CircuitBlock.java
@@ -12,7 +12,6 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;

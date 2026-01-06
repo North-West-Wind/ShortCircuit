00f9d42f930353203a7be50f6e83be10d6e957d2
North-West-Wind <wsyn148@gmail.com>
2026-01-06 16:55:05 +0800

    fix: correctly detect circuit updates in truth assigner

--- common/src/main/java/in/northwestw/shortcircuit/registries/blocks/TruthAssignerBlock.java
+++ common/src/main/java/in/northwestw/shortcircuit/registries/blocks/TruthAssignerBlock.java
@@ -101,11 +101,10 @@ public class TruthAssignerBlock extends HorizontalDirectionalBlock implements En
     @Override
     public void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston) {
         super.neighborChanged(state, level, pos, neighborBlock, neighborPos, movedByPiston);
-        if (pos.above().equals(neighborPos) && level.getBlockEntity(pos) instanceof TruthAssignerBlockEntity blockEntity) {
-            if (!blockEntity.isWorking()) blockEntity.setErrorCode(1, level.getBlockState(neighborPos).isAir());
-            else if (level.getBlockState(neighborPos).is(Blocks.CIRCUIT.get())) {
-                blockEntity.checkAndRecord();
-            }
+        BlockPos above = pos.above();
+        if (level.getBlockEntity(pos) instanceof TruthAssignerBlockEntity blockEntity && neighborBlock == Blocks.CIRCUIT.get() && level.getBlockState(above).is(Blocks.CIRCUIT.get())) {
+            if (!blockEntity.isWorking()) blockEntity.setErrorCode(1, level.getBlockState(above).isAir());
+            else blockEntity.checkAndRecord();
         }
     }
 }
